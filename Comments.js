70329b357b2ad48270c176f10e11161a4d397650
debasispaul <idebasispaul@gmail.com>
2022-07-30 23:50:57 -0700

    Comment Folder Added

---
+++ Comments.js
@@ -0,0 +1,12 @@
+var number = 5; // in-line comment
+
+// multi-line comment
+/*
+this is a multi-line comment
+7777777777777777
+0000000000000000
+8888888888888888
+*/
+
+number = 8; // No Comment
+
