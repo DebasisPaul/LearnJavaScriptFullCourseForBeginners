422a544510ff58e24c3302576c4a802732b33bbd
debasispaul <idebasispaul@gmail.com>
2022-07-31 00:01:22 -0700

    Data Type file added

---
+++ Datatypes&Variable.js
@@ -0,0 +1,8 @@
+/*Data Types:
+undefiend (A variable that could be set anything), null (nothing), boolean (True or false), symbol, string (Text), symbol, number (number) and objects (Can store a lot of different key value pairs)
+*/
+
+var myName = "Beau"
+myName = 8
+let ourName  = "freeCodeCamp"
+const pi = 3.14
